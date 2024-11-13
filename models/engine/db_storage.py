213a7298bf04d8fd8726a6619e84042db17e0092
clay <rpctropic13@gmail.com>
2024-11-13 04:21:16 -0600

    pepcheck

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,7 +37,9 @@ class DBStorage:
             HBNB_MYSQL_PWD,
             HBNB_MYSQL_HOST,
             HBNB_MYSQL_DB),
-            pool_pre_ping=True),
+            pool_pre_ping=True)
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """query on the current database session"""
