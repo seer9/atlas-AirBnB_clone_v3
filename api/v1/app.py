4f4631d33a85d917ef024117263e7072686738c4
clay <rpctropic13@gmail.com>
2024-11-12 22:52:09 -0600

    pepcheck

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,11 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     from os import getenv
