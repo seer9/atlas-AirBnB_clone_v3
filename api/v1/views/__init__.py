0750209d222350c435ccb68b275a1d6d2e57a19f
clay <rpctropic13@gmail.com>
2024-11-12 23:24:51 -0600

    state routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
+"""create blueprint"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
