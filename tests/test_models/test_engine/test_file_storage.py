e6f6d7f6dd89cbe60d153573fa9a6d5eca967291
clay <rpctropic13@gmail.com>
2024-11-13 04:51:09 -0600

    file storage checks

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,145 +1,131 @@
 #!/usr/bin/python3
-"""
-Contains the TestFileStorageDocs classes
-"""
-import unittest
+'''
+    Testing the file_storage module.
+'''
+
 import os
+import time
+import json
+import unittest
+import models
 from models import storage
-from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
 from models.state import State
-import inspect
-import pep8
+from models.engine.file_storage import FileStorage
 
-FileStorage = FileStorage
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+db = os.getenv("HBNB_TYPE_STORAGE")
 
 
-class TestFileStorageDocs(unittest.TestCase):
-    """Tests to check the documentation and style of FileStorage class"""
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.fs_f = inspect.getmembers(FileStorage, inspect.isfunction)
+@unittest.skipIf(db == 'db', "Testing DBstorage only")
+class testFileStorage(unittest.TestCase):
+    """Testing the FileStorage class"""
 
-    def test_pep8_conformance_file_storage(self):
-        """Test that models/engine/file_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_file_storage(self):
-        """Test tests/test_models/test_file_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_engine/test_file_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-
-class TestFileStorage(unittest.TestCase):
-    """Test the FileStorage class"""
 
     def setUp(self):
-        """Set up for the tests"""
+        """Initializing classes"""
         self.storage = FileStorage()
-        self.storage.reload()
+        self.my_model = BaseModel()
 
     def tearDown(self):
-        """Tear down after the tests"""
+        """ Cleaning up. """
+
         try:
             os.remove("file.json")
         except FileNotFoundError:
             pass
 
     def test_all_return_type(self):
-        """Test that all returns a dictionary"""
-        self.assertIsInstance(self.storage.all(), dict)
+        """ Tests the data type of the return value of the all method. """
+        storage_all = self.storage.all()
+        self.assertIsInstance(storage_all, dict)
 
     def test_new_method(self):
-        """Test the new method"""
-        obj = BaseModel()
-        self.storage.new(obj)
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.assertIn(key, self.storage.all())
+        """ Tests that the new method sets the right key and value pair
+        	in the FileStorage.__object attribute 
+        """
+        self.storage.new(self.my_model)
+        key = str(self.my_model.__class__.__name__ + "." + self.my_model.id)
+        self.assertTrue(key in self.storage._FileStorage__objects)
 
     def test_objects_value_type(self):
-        """Test that the values in __objects are instances of BaseModel"""
-        obj = BaseModel()
-        self.storage.new(obj)
-        self.assertIsInstance(list(self.storage.all().values())[0], BaseModel)
+        """ Tests that the type of value contained in the FileStorage.__object
+            is of type obj.__class__.__name__ """
+        self.storage.new(self.my_model)
+        key = str(self.my_model.__class__.__name__ + "." + self.my_model.id)
+        val = self.storage._FileStorage__objects[key]
+        self.assertIsInstance(self.my_model, type(val))
 
     def test_save_file_exists(self):
-        """Test that save creates a file"""
+        """ Tests that a file gets created with the name file.json"""
         self.storage.save()
-        self.assertTrue(os.path.exists("file.json"))
+        self.assertTrue(os.path.isfile("file.json"))
 
     def test_save_file_read(self):
-        """Test that save writes to the file"""
+        """Testing the contents of the files inside the file.json"""
         self.storage.save()
-        with open("file.json", "r") as f:
-            content = f.read()
-        self.assertIn("BaseModel", content)
+        self.storage.new(self.my_model)
+
+        with open("file.json", encoding="UTF8") as fd:
+            content = json.load(fd)
+
+        self.assertTrue(type(content) is dict)
 
     def test_the_type_file_content(self):
-        """Test the type of the content in the file"""
-        obj = BaseModel()
-        self.storage.new(obj)
+        """testing the type of the contents inside the file."""
         self.storage.save()
-        with open("file.json", "r") as f:
-            content = f.read()
+        self.storage.new(self.my_model)
+
+        with open("file.json", encoding="UTF8") as fd:
+            content = fd.read()
+
         self.assertIsInstance(content, str)
 
-    def test_reload_without_file(self):
-        """Test reload without a file"""
+    def test_reaload_without_file(self):
+        """ Tests that nothing happens when file.json does not exists
+            and reload is called
+        """
+
         try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
-        self.storage.reload()
-        self.assertEqual(self.storage.all(), {})
+            self.storage.reload()
+            self.assertTrue(True)
+        except:
+            self.assertTrue(False)
 
     def test_delete(self):
-        """Test the delete method"""
-        obj = BaseModel()
-        self.storage.new(obj)
-        self.storage.save()
-        self.storage.delete(obj)
-        self.assertNotIn(obj, self.storage.all().values())
+        """ Test delete method """
+        fs = FileStorage()
+        new_state = State()
+        fs.new(new_state)
+        state_id = new_state.id
+        fs.save()
+        fs.delete(new_state)
+        with open("file.json", encoding="UTF-8") as fd:
+            state_dict = json.load(fd)
+        for k, v in state_dict.items():
+            self.assertFalse(state_id == k.split('.')[1])
 
     def test_model_storage(self):
-        """Test that the model is stored correctly"""
-        obj = BaseModel()
-        self.storage.new(obj)
-        self.storage.save()
-        self.assertIn(obj, self.storage.all().values())
+        """ Test State model in Filestorage """
+        self.assertTrue(isinstance(storage, FileStorage))
 
     def test_get(self):
-        """Test the get method"""
-        obj = BaseModel()
-        obj_id = obj.id
-        self.storage.new(obj)
-        self.storage.save()
-        self.assertEqual(self.storage.get(BaseModel, obj_id), obj)
-        self.assertIsNone(self.storage.get(BaseModel, "nonexistent_id"))
+        """ Test if get method retrieves obj requested """
+        new_state = State(name="NewYork")
+        storage.new(new_state)
+        key = "State.{}".format(new_state.id)
+        result = storage.get("State", new_state.id)
+        self.assertTrue(result.id, new_state.id)
+        self.assertIsInstance(result, State)
 
     def test_count(self):
-        """Test the count method"""
-        initial_count = self.storage.count()
-        obj = BaseModel()
-        self.storage.new(obj)
-        self.storage.save()
-        self.assertEqual(self.storage.count(), initial_count + 1)
-        self.assertEqual(self.storage.count(BaseModel), initial_count + 1)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        """ Test if count method returns the correct number of objects """
+        old_count = storage.count("State")
+        new_state1 = State(name="NewYork")
+        storage.new(new_state1)
+        new_state2 = State(name="Virginia")
+        storage.new(new_state2)
+        new_state3 = State(name="California")
+        storage.new(new_state3)
+        self.assertEqual(old_count + 3, storage.count("State"))
 
 
