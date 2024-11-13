8d0ef59aa99b8345f49b5433dae138dd03ebb3c0
clay <rpctropic13@gmail.com>
2024-11-13 01:45:53 -0600

    changed post method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,16 +34,15 @@ def delete_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a State"""
-    if not request.get_json():
+    if not request.is_json:
         abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
+    data = request.get_json()
+    if 'name' not in data:
         abort(400, description="Missing name")
-    states = []
-    new_state = State(name=request.json['name'])
+    new_state = State(name=data['name'])
     storage.new(new_state)
     storage.save()
-    states.append(new_state.to_dict())
-    return jsonify(states[0]), 201
+    return jsonify(new_state.to_dict()), 201
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
