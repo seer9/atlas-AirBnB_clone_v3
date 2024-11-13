5dcc2ba439830360371b649b1fc5ddd95bae7067
clay <rpctropic13@gmail.com>
2024-11-12 23:06:22 -0600

    404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Main API module """
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -11,6 +11,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
