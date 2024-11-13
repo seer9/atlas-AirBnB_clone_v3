d1be204620d6aff006f00ffb94043f8a19bd4054
clay <rpctropic13@gmail.com>
2024-11-12 23:28:02 -0600

    updated routes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,13 +6,13 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'])
 def get_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
     """Retrieves a State object"""
     state = storage.get(State, state_id)
@@ -20,7 +20,7 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """Deletes a State object"""
     state = storage.get(State, state_id)
@@ -30,7 +30,7 @@ def delete_state(state_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=['POST'])
 def create_state():
     """Creates a State"""
     if not request.json:
@@ -43,7 +43,7 @@ def create_state():
     storage.save()
     return jsonify(new_state.to_dict()), 201
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
     """Updates a State object"""
     state = storage.get(State, state_id)
