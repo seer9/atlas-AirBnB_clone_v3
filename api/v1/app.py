710576545a1bc276c17d3ed6d57b606d2f58b730
clay <rpctropic13@gmail.com>
2024-11-12 22:37:44 -0600

    initial api

---
+++ api/v1/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    from os import getenv
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
